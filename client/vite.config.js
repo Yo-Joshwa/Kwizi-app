c05c71bc25dc82d29e3021c70fd1d6556e46b207
Yo-Joshwa <shivambaberwal123@gmail.com>
2025-07-14 22:10:16 +0530

    updated vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": `${import.meta.env.VITE_API_URL}`,
     },
   },
   resolve: {
