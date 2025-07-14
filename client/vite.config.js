3c537e6853051254de25024bd13d5c94194f4077
Yo-Joshwa <shivambaberwal123@gmail.com>
2025-07-14 22:16:31 +0530

    update client side

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,18 +2,23 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
 
-// https://vite.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api": `${import.meta.env.VITE_API_URL}`,
+      "/api": {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
